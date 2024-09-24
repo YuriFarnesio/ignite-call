89877fb6997887d8484643e6b6524258cc335982
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-24 12:30:41 -0300

    feat: add react hook form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,13 +1,32 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from '@phosphor-icons/react'
+import { z } from 'zod'
 
+import { useForm } from 'react-hook-form'
 import { Form } from './styles'
 
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       {/* @ts-expect-error: Unreachable code error */}
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
 
       <Button size="sm" type="submit">
         Reservar
