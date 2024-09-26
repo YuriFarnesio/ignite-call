f3308660b18086e327dda8735c924eb97160b7b5
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-26 02:21:33 -0300

    fix: change connect calendar component name

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router'
 import { Container, Header } from '../styles'
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 
-export default function Register() {
+export default function ConnectCalendar() {
   const router = useRouter()
   const session = useSession()
 
