3e904abc5cdde3cf5d405b311cc384022d67987e
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-10-02 15:38:33 -0300

    finish blocked dates api route

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -27,6 +27,7 @@ type CalendarWeeks = CalendarWeek[]
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 interface CalendarProps {
@@ -69,7 +70,7 @@ export function Calendar({ onDateSelected }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month') + 1,
+          month: String(currentDate.get('month') + 1).padStart(2, '0'),
         },
       })
 
@@ -119,7 +120,8 @@ export function Calendar({ onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
